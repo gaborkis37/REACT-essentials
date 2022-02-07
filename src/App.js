8b482c292443257feaa13d9e122ab0a0484394b8
Gabor Kis <gaborkis37@gmai.com>
2022-02-07 22:24:20 +0100

    fetching data from external API

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
+// https://api.github.com/users/gaborkis37
+
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    console.log(`It's ${emotion} around here`);
-  }, [emotion]);
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/todos/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
 
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`);
-  }, [secondary]);
+  if (loading) return <h1>Loading...</h1>;
 
-  return (
-    <>
-      <h1>
-        Current emotion is {emotion} and {secondary}
-      </h1>
-      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-      <button onClick={() => setEmotion("happy")}>Happy</button>
-      <button onClick={() => setEmotion("enthusiastic")}>Enthusiastic</button>
-      <button onClick={() => setSecondary("crabby")}>Crabby</button>
-    </>
-  );
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
+  if (!data) return null;
+
+  if (data) {
+    return (
+      <div>
+        <h1>hello {data.id}</h1>
+        <h1>TITLE: {data.title}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
